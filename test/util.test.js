26fa0c4998e163d212947c229ee0de312b5c9ef2
Haider Ali <rmha62@gmail.com>
2025-04-25 19:37:29 +0500

    style: use prettier for formating

--- test/util.test.js
+++ test/util.test.js
@@ -2,20 +2,19 @@
  * Module requirements.
  */
 
-require('should');
+require("should")
 
-var { strlen } = require('../lib/utils');
+var { strlen } = require("../lib/utils")
 
 /**
  * Tests.
  */
 
-
- module.exports = {
-    'test strlen with string argument': function() {
-        strlen('hello\nme').should.equal(5);
-    },
-    'test strlen with number argument': function() {
-        strlen(5).should.equal(1);
-    },
- }
+module.exports = {
+  "test strlen with string argument": function () {
+    strlen("hello\nme").should.equal(5)
+  },
+  "test strlen with number argument": function () {
+    strlen(5).should.equal(1)
+  },
+}
